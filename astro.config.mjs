3521136ee1f6f540af13a195f60f3b227259340b
Conor Svensson <conor10@gmail.com>
2026-09-15 15:39:21 +0100

    Remove obsolete books redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://conorsvensson.com',
-  redirects: {
-    '/books/': '/reading/',
-  },
   integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
